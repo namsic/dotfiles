6e15463106d58694d72f796dac2a82d38ebef5f3
Namjae Kim <namjae.kim@namsic.dev>
2025-05-08 16:34:31 +0900

    Add nvim config

---
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "rust",
+      "yaml",
+    },
+  },
+}
+
